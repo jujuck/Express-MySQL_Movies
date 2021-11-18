e94e6cbccbc06096ca5e4ec04f7228a454a69627
Julien ichard WCS <julien.y.richard@gmail.com>
2021-11-18 11:25:22 +0100

    Step 5: ajotu d'une route pour un seul film

--- app.js
+++ app.js
@@ -12,6 +12,20 @@ connexion.connect((err) => {
   }
 });
 
+app.get('/movies/:id', (req, res) => {
+  const movieId = req.params.id;
+  
+  connexion.promise().query(
+    'SELECT * FROM movies WHERE id = ?',
+    [movieId])
+    .then((result) => {
+      if (result[0].length) res.status(201).json(result[0]);
+      else res.status(404).send('Movie not found');
+    }).catch((err)=> {
+      res.send('Error retrieving data from database');
+    })
+});
+
 app.get('/movies', (req, res) => {
   connexion.promise().query('SELECT * FROM movies')
     .then((result) => {
