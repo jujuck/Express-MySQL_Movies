a84e69950d087a9b3d5932b41088db22427a8bde
Julien ichard WCS <julien.y.richard@gmail.com>
2021-12-07 11:53:23 +0100

    Part 2, step 11, adding validation for the put route

--- models/movies.js
+++ models/movies.js
@@ -1,6 +1,19 @@
+const Joi = require('joi');
 const connexion = require('../db-config');
 
 const db = connexion.promise();
+
+const checkMoviesFields = (data, create = false) => {
+  const presence = create ? 'required' : 'optional';
+  return Joi.object({
+    id: Joi.number().presence('optional'),
+    title: Joi.string().max(250).presence(presence),
+    synopsis: Joi.string().presence(presence),
+    year: Joi.number().integer().min(1888).presence(presence),
+    genre: Joi.string().max(100).presence(presence),
+    duration: Joi.number().integer().min(1).presence(presence),
+  }).validate(data, { abortEarly: false }).error;
+}
 
 const findMany = () => {
   return db.query('SELECT * FROM movies')
@@ -40,5 +53,6 @@ module.exports = {
   findMany,
   createOne,
   updateOne,
-  deleteOne
+  deleteOne,
+  checkMoviesFields
 };
--- routes/movies.js
+++ routes/movies.js
@@ -2,7 +2,7 @@ const moviesRouter = require('express').Router();
 const Movies = require('../models/movies');
 
 moviesRouter.get('/', (req, res) => {
-  Movies.findMany()
+  Movies.findMany(req.query)
     .then((movies) => {
       if (movies) {
         res.status(200).json(movies);
@@ -30,23 +30,33 @@ moviesRouter.get('/:id', (req, res) => {
 });
 
 moviesRouter.post('/', (req, res) => {
-  Movies.createOne(req.body)
-    .then((result) => {
-      res.send({ succes: 'Movie successfully save', data: result });
-    })
-    .catch((err) => {
-      res.send('Error saving the movie');
-    })
+  const error = Movies.checkMoviesFields(req.body, true);
+  if (error) {
+    res.status(401).send({ msg: 'Invalid fields', error })
+  } else {
+    Movies.createOne(req.body)
+      .then((result) => {
+        res.send({ succes: 'Movie successfully save', data: result });
+      })
+      .catch((err) => {
+        res.send('Error saving the movie');
+      })
+  }
 });
 
 moviesRouter.put("/:id", (req, res) => {
-  Movies.updateOne(req.params.id, req.body)
-    .then((result) => {
-      res.send({ success: 'Movie updated successfully', data: result })
-    })
-    .catch((err) => {
-      res.send("Error updating the movie")
-    })
+  const error = Movies.checkMoviesFields(req.body, true);
+  if (error) {
+    res.status(401).send({ msg: 'Invalid fields', error })
+  } else {
+    Movies.updateOne(req.params.id, req.body)
+      .then((result) => {
+        res.send({ success: 'Movie updated successfully', data: result })
+      })
+      .catch((err) => {
+        res.send("Error updating the movie")
+      })
+  }
 });
 
 moviesRouter.delete("/:id", (req, res) => {
